1a05b8c4ebcf9c8165b004ce3b26604230d372ad
zehuama <zehua046@gmail.com>
2017-05-29 16:51:21 +0800

    touch app/views/carts/checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,11 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       delete :clean
-      post :checkout 
+      post :checkout
     end
   end
 
+  resources :orders
   resources :cart_items
 
   devise_for :users
